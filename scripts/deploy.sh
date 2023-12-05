b84b69133e3fe17da62d4f25aeaaaeb903d88e50
Park Hyeongyu <park970406@me.com>
2023-12-05 21:23:53 +0900

    update cicd pipeline

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,23 @@
-#!/usr/bin/env bash
+REPOSITORY1=/home/ubuntu
+REPOSITORY=/home/ubuntu/app
+PROJECT_NAME=plug
 
-REPOSITORY=/home/ubuntu/plug
-cd $REPOSITORY
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep java | awk '{print $1}')
 
-APP_NAME=plug
-JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '-0.0.1-SNAPSHOT.jar' | tail -n 1)
-JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
-
-CURRENT_PID=$(pgrep -f $APP_NAME)
-
-if [ -z $CURRENT_PID ]
-then
-  echo "> 종료할 애플리케이션이 없습니다."
+if [ -z $CURRENT_PID ]; then
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
-echo "> Deploy - $JAR_PATH "
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "> 새 어플리케이션 배포"
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> $JAR_NAME에 실행권한 추가"
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+nohup java -jar $JAR_NAME > $REPOSITORY1/nohup.out 2>&1 &
