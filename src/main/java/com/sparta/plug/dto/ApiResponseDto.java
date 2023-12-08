98e441bc5d0618dfab5b370966eb0dfea5552351
Park Hyeongyu <park970406@me.com>
2023-12-09 03:00:21 +0900

    update CI/CD pipeline

--- src/main/java/com/sparta/plug/dto/ApiResponseDto.java
+++ src/main/java/com/sparta/plug/dto/ApiResponseDto.java
@@ -12,7 +12,7 @@ import lombok.Setter;
 public class ApiResponseDto {
     private String msg;
     private Integer statusCode;
-
+// gradle
     public ApiResponseDto(String msg, Integer statusCode) {
         this.msg = msg;
         this.statusCode = statusCode;
